f9b9d1371c89fb5e547db332af030230fe01b610
18PatZ <18patz@students.harker.org>
2015-05-25 08:44:00 -0700

    Update code.js

--- code.js
+++ code.js
@@ -309,7 +309,12 @@ function AI(node){
         }
         
         sa(node,"fire",1);
-        sa(node,"vy",-1);
+        if(collision(node,targetNode,false)){
+            sa(node,"vy",0);
+        }else {
+            sa(node,"vy",-1);
+        }
+        
         
 }
 
