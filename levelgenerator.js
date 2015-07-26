3f0849d14cacd8e5510bd71cd87cbf9e6bb8653f
18praveenb <Praveen@Shire-2.local>
2015-07-26 10:42:04 -0700

    level generator returns a level now
    no enemies yet

--- levelgenerator.js
+++ levelgenerator.js
@@ -1,1 +1,11 @@
 //Praveen's level generator
+
+function generateRandomLevel(parameters) {
+    var array = [];
+    array.push({x:0, y:0, name:"PlayerSpawn"})
+    var cx = 0;
+    var cy = 0;
+    for (var i = 0; i < 100; ++i) if (Math.random() < 0.5) ++cx; else ++cy;
+    array.push({x:cx, y:cy, name:"Exit"})
+    return array
+}
